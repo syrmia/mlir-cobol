19befcc6df7dd4dff9a3307cc1a3440726c07d55
Ana Marija <ana.marija.ognjenovic@htecgroup.com>
2026-01-23 16:48:44 +0100

    examples: Add examples

--- examples/display_num.cbl
+++ examples/display_num.cbl
@@ -5,7 +5,8 @@
        WORKING-STORAGE SECTION.
        01 WS-AGE PIC 9(3) VALUE 25.
        01 WS-NAME PIC X(4) VALUE "Duke".
-       01 WS PIC 9(1).
        PROCEDURE DIVISION.
        Main-Process.
+           DISPLAY WS-AGE.
+           DISPLAY WS-NAME.
            STOP RUN.
