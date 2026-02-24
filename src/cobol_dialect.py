473a9f340e7ff697079cd4c66bbfe9cbdbcf506d
Ana Marija <ana.marija.ognjenovic@htecgroup.com>
2026-02-24 14:59:20 +0100

    cobol: Add cobol.record type for group items - structs

--- src/cobol_dialect.py
+++ src/cobol_dialect.py
@@ -12,8 +12,9 @@ from xdsl.irdl import (
     prop_def,
     region_def,
     result_def,
-    var_operand_def
+    var_operand_def,
 )
+
 
 # ─────────────────────────────────────────────────────────────────────────────
 #  Type attributes
@@ -22,38 +23,50 @@ from xdsl.irdl import (
 class CobolBoolType(ParametrizedAttribute, TypeAttribute):
     name = "cobol.bool"
 
+
 @irdl_attr_definition
 class CobolStringType(ParametrizedAttribute, TypeAttribute):
-    name   = "cobol.string"
+    name = "cobol.string"
     length: IntegerAttr
+
 
 @irdl_attr_definition
 class CobolDecimalType(ParametrizedAttribute, TypeAttribute):
-    name   = "cobol.decimal"
+    name = "cobol.decimal"
     digits: IntegerAttr
     scale: IntegerAttr
+
+
+@irdl_attr_definition
+class CobolRecordType(ParametrizedAttribute, TypeAttribute):
+    name = "cobol.record"
+    record_name: StringAttr
+
 
 # ─────────────────────────────────────────────────────────────────────────────
 #  Operation definitions
 # ─────────────────────────────────────────────────────────────────────────────
 @irdl_op_definition
 class FunctionOp(IRDLOperation):
-    name          = "cobol.func"
-    sym_name      = prop_def(StringAttr)
+    name = "cobol.func"
+    sym_name = prop_def(StringAttr)
     function_type = prop_def(FunctionType)
-    body          = region_def("single_block")
+    body = region_def("single_block")
+
 
 @irdl_op_definition
 class AcceptOp(IRDLOperation):
-    name     = "cobol.accept"
+    name = "cobol.accept"
     args = operand_def(StringAttr)
+
 
 @irdl_op_definition
 class AddOp(IRDLOperation):
-    name   = "cobol.add"
-    lhs    = operand_def()
-    rhs    = operand_def()
+    name = "cobol.add"
+    lhs = operand_def()
+    rhs = operand_def()
     result = result_def()
+
 
 @irdl_op_definition
 class AndIOp(IRDLOperation):
@@ -62,12 +75,14 @@ class AndIOp(IRDLOperation):
     rhs = operand_def()
     result = result_def()
 
+
 @irdl_op_definition
-class ClassOp(IRDLOperation):
-    name = "cobol.class"
-    class_name = prop_def(StringAttr)
+class StructOp(IRDLOperation):
+    name = "cobol.struct"
+    struct_name = prop_def(StringAttr)
     body = region_def()
     result = result_def()
+
 
 @irdl_op_definition
 class CmpIOp(IRDLOperation):
@@ -77,23 +92,27 @@ class CmpIOp(IRDLOperation):
     rhs = operand_def()
     result = result_def()
 
+
 @irdl_op_definition
 class ConstantOp(IRDLOperation):
-    name   = "cobol.constant"
-    value  = prop_def(StringAttr | IntegerAttr)
+    name = "cobol.constant"
+    value = prop_def(StringAttr | IntegerAttr)
     result = result_def()
+
 
 @irdl_op_definition
 class DeclareOp(IRDLOperation):
-    name     = "cobol.declare"
+    name = "cobol.declare"
     value = prop_def(IntegerAttr | StringAttr)
     level = prop_def(StringAttr)
-    result   = result_def()
+    result = result_def()
+
 
 @irdl_op_definition
 class DisplayOp(IRDLOperation):
     name = "cobol.display"
     args = var_operand_def()
+
 
 @irdl_op_definition
 class IfOp(IRDLOperation):
@@ -102,25 +121,29 @@ class IfOp(IRDLOperation):
     then_region = region_def()
     else_region = region_def()
 
+
 @irdl_op_definition
 class IsOp(IRDLOperation):
     name = "cobol.is"
-    var  = operand_def()
+    var = operand_def()
     kind = prop_def(StringAttr)
     is_positive = prop_def(StringAttr)
     result = result_def()
 
+
 @irdl_op_definition
 class MoveOp(IRDLOperation):
     name = "cobol.move"
-    src  = operand_def()
-    dst  = operand_def()
+    src = operand_def()
+    dst = operand_def()
+
 
 @irdl_op_definition
 class NotOp(IRDLOperation):
     name = "cobol.not"
     op = operand_def()
     result = result_def()
+
 
 @irdl_op_definition
 class OrIOp(IRDLOperation):
@@ -129,15 +152,18 @@ class OrIOp(IRDLOperation):
     rhs = operand_def()
     result = result_def()
 
+
 @irdl_op_definition
 class SetOp(IRDLOperation):
-    name     = "cobol.set"
+    name = "cobol.set"
     sym_name = prop_def(StringAttr)
     result = result_def()
+
 
 @irdl_op_definition
 class StopRunOp(IRDLOperation):
     name = "cobol.stop"
+
 
 # ─────────────────────────────────────────────────────────────────────────────
 #  Dialect registration
@@ -157,11 +183,8 @@ COBOL = Dialect(
         MoveOp,
         OrIOp,
         StopRunOp,
-        SetOp
+        SetOp,
+        StructOp,
     ],
-    [
-        CobolBoolType,
-        CobolStringType,
-        CobolDecimalType
-    ],
+    [CobolBoolType, CobolStringType, CobolDecimalType, CobolRecordType],
 )
