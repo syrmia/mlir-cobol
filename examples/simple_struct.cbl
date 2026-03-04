8ffb803fe34e0b29002cc2e8296e68a944eb1f68
Ana Marija <ana.marija.ognjenovic@htecgroup.com>
2026-03-04 13:00:15 +0100

    examples: edit example

--- examples/simple_struct.cbl
+++ examples/simple_struct.cbl
@@ -8,6 +8,9 @@
                   02 UID PIC 9(5) VALUE 12345.
                   02 DESC PIC X(25) VALUE 'TEST TRANSACTION'.
 
+                01 FIRST-VAR  PIC 9(2).
+                01 SECOND-VAR PIC S9(2)V9(2).
+
             PROCEDURE DIVISION.
               *> print the record we are writing
               DISPLAY 'WRITING RECORD: 'TRANSACTION-RECORD.
